5a132a85d5a099cb2f6f101c81913585b4601903
ʍօռʊ <46643805+Rahul-Kmr@users.noreply.github.com>
2021-04-06 23:46:10 +0530

    Add files via upload

---
+++ program_to_add_two_number.py
@@ -0,0 +1,9 @@
+
+number1 = input ("first number")
+number2 = input("second number")
+
+#adding two numbers
+
+sum = float(number1) + float(number2)
+
+print("the sum of {0} and {1} is {2}". format(number1, number2, sum))
